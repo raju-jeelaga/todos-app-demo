d3c3caafad2e22023b142d5b2cf721775aac25b8
Raju Jeelaga <rajujeelaga@gmail.com>
2020-03-10 23:40:07 +0530

    todos data get through API key
    todos data get through API key

--- src/app/todos/todos-list/todos-list.component.ts
+++ src/app/todos/todos-list/todos-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-todos-list',
   templateUrl: './todos-list.component.html',
   styleUrls: ['./todos-list.component.css']
 })
 export class TodosListComponent implements OnInit {
-
-  constructor() { }
+  isLoading:boolean = false;
+  todos: any[] = []
+  constructor(private http: HttpClient ) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.http.get('https://todos-api-dev.herokuapp.com/todos').subscribe((data:any[]) =>{
+      this.isLoading = false;
+       this.todos = data;
+       console.log("Dada", this.todos);
+    });
   }
 
 }
