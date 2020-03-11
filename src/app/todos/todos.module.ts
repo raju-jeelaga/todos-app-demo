de450f0b2ece404723eb59d6a6bcdf7e7ac30ef6
Raju Jeelaga <rajujeelaga@gmail.com>
2020-03-11 08:43:43 +0530

    todos app list is done
    todos app list is done

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodosListComponent } from './todos-list/todos-list.component';
 import { AddEditTodoComponent } from './add-edit-todo/add-edit-todo.component';
+import { RouterModule} from '@angular/router';
 
 
 
 @NgModule({
   declarations: [TodosListComponent, AddEditTodoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [TodosListComponent]
 })
